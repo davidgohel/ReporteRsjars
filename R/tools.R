08a375140465321b8163ea5a9c693793b81412e4
dgohel <david.gohel@ardata.fr>
2017-06-22 22:34:43 +0200

    fix for ubuntu

--- R/tools.R
+++ R/tools.R
@@ -1,6 +1,6 @@
-.onLoad= function(libname, pkgname){
-  path_ <- system.file(package = "ReporteRsjars", "java")
-  jars <- grep(".*\\.jar", list.files(path_, full.names = TRUE), TRUE, value = TRUE)
-  .jaddClassPath(path=jars)
-  invisible()
-}
+# .onLoad= function(libname, pkgname){
+#   path_ <- system.file(package = "ReporteRsjars", "java")
+#   jars <- grep(".*\\.jar", list.files(path_, full.names = TRUE), TRUE, value = TRUE)
+#   .jaddClassPath(path=jars)
+#   invisible()
+# }
